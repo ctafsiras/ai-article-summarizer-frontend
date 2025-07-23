a6e50fbaa3b0fa5dd2f00eeb213f74de91f4aba7
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2025-07-23 22:03:25 +0600

    url update

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const BASE_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
